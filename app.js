b523ce1f80b0791f89b2579af219fc518f49f14e
Md Amaan <mdamaan853@gmail.com>
2021-06-16 21:32:48 +0530

    port

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const fs = require("fs")
 app.use(cors());
 app.use(express.json())
 
+const PORT = 4000
+
 const userRouter =require('./controller/user/users.router')
 const courseRouter =require('./controller/course/course.router')
 const orderRouter =require('./controller/order/order.router')
@@ -18,6 +20,6 @@ app.use('/user',userRouter)
 app.use('/course',courseRouter)
 app.use('/order',orderRouter)
 
-app.listen(4000, () => {
-    console.log('HTTPS Server running on port 4000');
+app.listen(PORT, () => {
+    console.log(`HTTPS Server running on port ${PORT}`);
 });
